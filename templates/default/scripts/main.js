bbbfeebdb49930be3cd9bb9d616d8464e3bf3ca1
William To <william.to@jam3.com>
2018-06-11 15:03:12 -0400

    Add some modules and minor setup code fix

--- templates/default/scripts/main.js
+++ templates/default/scripts/main.js
@@ -3,6 +3,7 @@ const art = (function () {
   let renderer, camera, scene;
 
   function mountRenderer() {
+    if (!Detector.webgl) Detector.addGetWebGLMessage();
     renderer =
       window.WebGLRenderingContext
       ? new THREE.WebGLRenderer()
@@ -14,18 +15,18 @@ const art = (function () {
   function initializeScene() {
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(
-      35,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
+    camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.z = 100;
+    scene.add(camera);
 
     let light = new THREE.AmbientLight(0xffffff);
-
-    scene.add(camera);
     scene.add(light);
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff);
+    scene.add(directionalLight);
+
+    // Event listeners
+    window.addEventListener('resize', onWindowResize, false);
   }
 
   function render() {
@@ -33,12 +34,16 @@ const art = (function () {
     window.requestAnimationFrame(render);
   }
 
+  function onWindowResize(e) {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
   // Demo ---
   function addBox() {
-    const box = new THREE.Mesh(
-      new THREE.BoxGeometry(20, 20, 20),
-      new THREE.MeshLambertMaterial({color: 0x9883E5})
-    );
+    let boxGeometry = new THREE.BoxGeometry(20, 20, 20);
+    let boxMaterial = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
+    
+    let box = new THREE.Mesh(boxGeometry, boxMaterial);
     scene.add(box);
   }
   // --------
